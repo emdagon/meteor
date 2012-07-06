49217c29b200c1c09e26528db19f59adec7ebf54
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-05 20:55:32 -0400

    fixed

--- packages/startup/startup_server.js
+++ packages/startup/startup_server.js
@@ -1,4 +1,6 @@
 Meteor.startup = function (callback) {
-  console.log('Meteor server successfully fetched and loaded (branch:mrt-test)');
   __meteor_bootstrap__.startup_hooks.push(callback);
 };
+__meteor_bootstrap__.startup_hooks.push(function() {
+  console.log('Meteor server successfully fetched and loaded (branch:mrt-test)');
+});
