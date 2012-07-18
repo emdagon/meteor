6a53c8bdfb297b3abe86c3985820795a34725408
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-18 00:56:36 -0400

    added trace

--- packages/startup/startup_server.js
+++ packages/startup/startup_server.js
@@ -1,4 +1,4 @@
 Meteor.startup = function (callback) {
   __meteor_bootstrap__.startup_hooks.push(callback);
-  console.log("Meteor is instrumented for meteorite test (ref)");
+  console.log("Meteor is instrumented for meteorite test (mrt-path-branch)");
 };
