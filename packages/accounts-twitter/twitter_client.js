8576b70c9ba62fd3a4f1767c3010a1a0cd241f36
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-28 23:56:41 -0400

    wip: cloned accounts-facebook and accounts-oauth2-helper

---
+++ packages/accounts-twitter/twitter_client.js
@@ -0,0 +1,27 @@
+(function () {
+  Meteor.loginWithTwitter = function () {
+    if (!Meteor.accounts.twitter._appId || !Meteor.accounts.twitter._appUrl)
+      throw new Meteor.accounts.ConfigError("Need to call Meteor.accounts.twitter.config first");
+
+    var state = Meteor.uuid();
+    var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
+    var display = mobile ? 'touch' : 'popup';
+
+    var scope = "email";
+    if (Meteor.accounts.twitter._options &&
+        Meteor.accounts.twitter._options.scope)
+      scope = Meteor.accounts.twitter._options.scope.join(',');
+
+    var loginUrl =
+          'https://www.facebook.com/dialog/oauth?client_id=' + Meteor.accounts.twitter._appId +
+          '&redirect_uri=' + Meteor.accounts.twitter._appUrl + '/_oauth1/twitter?close' +
+          '&display=' + display + '&scope=' + scope + '&state=' + state;
+
+    Meteor.accounts.oauth1.initiateLogin(state, loginUrl);
+  };
+
+})();
+
+
+
+
