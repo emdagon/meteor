0ce983b17c8d7cc86d436190e33491f23b42d0da
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-29 18:35:35 -0400

    moved oauth1 lib to it's own package

--- packages/accounts-oauth1-helper/package.js
+++ packages/accounts-oauth1-helper/package.js
@@ -5,6 +5,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('accounts', ['client', 'server']);
+  api.use('oauth1', 'server');
 
   api.add_files('oauth1_common.js', ['client', 'server']);
   api.add_files('oauth1_server.js', 'server');
@@ -12,6 +13,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use('accounts-oauth1-helper', 'server');
-  api.add_files("oauth1_tests.js", 'server');
+  // XXX Fix these!
+  // api.use('accounts-oauth1-helper', 'server');
+  // api.add_files("oauth1_tests.js", 'server');
 });
