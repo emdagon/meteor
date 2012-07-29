c55a2b46ba63be37521be2615ae738b1aa835100
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-29 16:34:00 -0400

    added comment

--- packages/accounts-oauth1-helper/oauth1_server.js
+++ packages/accounts-oauth1-helper/oauth1_server.js
@@ -89,19 +89,19 @@ var querystring = __meteor_bootstrap__.require("querystring");
 
       // Get or create user id
       var oauthResult = service.handleOauthRequest(oauth);
-
+      
       if (oauthResult) { // could be null if user declined permissions
         var userId = Meteor.accounts.updateOrCreateUser(oauthResult.options, oauthResult.extra);
-
+      
         // Generate and store a login token for reconnect
         // XXX this could go in accounts_server.js instead
         var loginToken = Meteor.accounts._loginTokens.insert({userId: userId});
-
+      
         // Store results to subsequent call to `login`
         Meteor.accounts.oauth1._loginResultForState[req.query.state] =
           {token: loginToken, id: userId};
       }
-
+      
       // We support ?close and ?redirect=URL. Any other query should
       // just serve a blank page
       if ('close' in req.query) { // check with 'in' because we don't set a value
@@ -155,7 +155,7 @@ var querystring = __meteor_bootstrap__.require("querystring");
         Authorization: authString
       }
     });
-
+    
     if (response.error)
       throw response.error;
 
